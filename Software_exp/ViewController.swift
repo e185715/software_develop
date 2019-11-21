7ebe4260798880e18a4fea42643eb16459664803
Shinto Higa <e185715@ie.u-ryukyu.ac.jp>
2019-11-21 10:10:48 +0900

    Initial Commit

---
+++ Software_exp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Software_exp
+//
+//  Created by Shinto Higa on 2019/11/21.
+//  Copyright © 2019 Shinto Higa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Software_expTests/Software_expTests.swift
@@ -0,0 +1,34 @@
+//
+//  Software_expTests.swift
+//  Software_expTests
+//
+//  Created by Shinto Higa on 2019/11/21.
+//  Copyright © 2019 Shinto Higa. All rights reserved.
+//
+
+import XCTest
+@testable import Software_exp
+
+class Software_expTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
